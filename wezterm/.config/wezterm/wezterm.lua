a1af8d5d089659e0a56fdafb23fe534901e0705b
kiefer <kiefx@pm.me>
2024-11-02 16:52:46 -0700

    Added a little cute toast notif to wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -18,4 +18,10 @@ config.keys = {
 	{ key = "t", mods = "CTRL|SHIFT", action = act.SpawnTab("DefaultDomain") },
 	{ key = "w", mods = "CTRL|SHIFT", action = act.CloseCurrentTab({ confirm = true }) },
 }
+
+-- Function to show Hello message
+wezterm.on("window-config-reloaded", function(window, pane)
+	window:toast_notification("Welcome", "Hello! Welcome to WezTerm!", nil, 3000)
+end)
+
 return config
