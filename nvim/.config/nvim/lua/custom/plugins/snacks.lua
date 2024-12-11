ae9700d4dd09b88db6bde51292cc90b44c198c77
Kiefer <kiefhay642@outlook.com>
2024-12-10 21:17:01 -0800

    Notifier changes

--- nvim/.config/nvim/lua/custom/plugins/snacks.lua
+++ nvim/.config/nvim/lua/custom/plugins/snacks.lua
@@ -2,14 +2,27 @@ return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
+  ---@type Snacks.Config
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    notifier = { enabled = true },
+    notifier = {
+      enabled = true,
+      style = 'fancy',
+    },
     quickfile = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
+  },
+  keys = {
+    {
+      '<leader>n',
+      function()
+        Snacks.notifier.show_history()
+      end,
+      desc = 'Notification History',
+    },
   },
 }
