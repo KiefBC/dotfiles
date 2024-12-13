2645f2d33961bc465214d31c90962213903fd00a
Kiefer <kiefhay642@outlook.com>
2024-12-12 23:40:47 -0800

    Settings modified

--- nvim/.config/nvim/lua/custom/plugins/comment.lua
+++ nvim/.config/nvim/lua/custom/plugins/comment.lua
@@ -1,11 +1,11 @@
 return {
   'numToStr/Comment.nvim',
+  event = { 'BufRead', 'BufNewFile' },
   dependencies = { 'joosepalviste/nvim-ts-context-commentstring' },
-		-- stylua: ignore
-		keys = {
-			{ '<Leader>V', '<Plug>(comment_toggle_blockwise_current)', mode = 'n', desc = 'Comment' },
-			{ '<Leader>V', '<Plug>(comment_toggle_blockwise_visual)', mode = 'x', desc = 'Comment' },
-		},
+  keys = {
+    { '<Leader>V', '<Plug>(comment_toggle_blockwise_current)', mode = 'n', desc = 'Comment' },
+    { '<Leader>V', '<Plug>(comment_toggle_blockwise_visual)', mode = 'x', desc = 'Comment' },
+  },
   opts = function(_, opts)
     local ok, tcc = pcall(require, 'ts_context_commentstring.integrations.comment_nvim')
     if ok then
