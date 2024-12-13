0719d8beb120565c0b2796f46cb2a50e0764e883
Kiefer <kiefhay642@outlook.com>
2024-12-12 23:41:23 -0800

    New plugin which will come in handy with sessions

---
+++ nvim/.config/nvim/lua/custom/plugins/auto-session.lua
@@ -0,0 +1,14 @@
+return {
+  'rmagatti/auto-session',
+  config = function()
+    local auto_session = require 'auto-session'
+    auto_session.setup {
+      auto_restore_enabled = false,
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+    }
+
+    local keymaps = vim.keymap
+    keymaps.set('n', '<leader>wr', '<cmd>SessionRestore<CR>', { desc = '[R]estore Session' })
+    keymaps.set('n', '<leader>ws', '<cmd>SessionSave<CR>', { desc = '[S]ave Session' })
+  end,
+}
