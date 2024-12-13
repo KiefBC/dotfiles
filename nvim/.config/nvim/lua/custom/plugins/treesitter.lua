1a37386eb9aa0d5a7736b5cdb1a9e5bac6e9d446
Kiefer <kiefhay642@outlook.com>
2024-12-12 23:40:01 -0800

    Added missing-fields, added co-dependency

--- nvim/.config/nvim/lua/custom/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/custom/plugins/treesitter.lua
@@ -4,10 +4,14 @@ return {
   event = { 'BufReadPre', 'BufNewFile' },
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
+    'windwp/nvim-ts-autotag',
   },
   config = function()
     local configs = require 'nvim-treesitter.configs'
     configs.setup {
+      diagnostics = {
+        disable = { 'missing-fields' },
+      },
       ensure_installed = { 'rust', 'svelte', 'javascript', 'html', 'css', 'bash', 'lua', 'tsx', 'typescript', 'json', 'tsx' },
       auto_install = true,
       highlight = {
@@ -15,6 +19,16 @@ return {
         additional_vim_regex_highlighting = true,
       },
       indent = { enable = true, disable = { 'python' } },
+      autotag = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-space>',
+          node_incremental = '<C-space>',
+          scope_incremental = false,
+          node_decremental = '<bs>',
+        },
+      },
     }
 
     -- This will highlight inline JSX on HTML files
