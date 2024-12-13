17e0349afeb1c447b10c1b1d55a63990bf3f8ba3
Kiefer <kiefhay642@outlook.com>
2024-12-12 23:41:11 -0800

    Customized some settings

--- nvim/.config/nvim/lua/custom/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/custom/plugins/autopairs.lua
@@ -7,7 +7,15 @@ return {
   -- Optional dependency
   dependencies = { 'hrsh7th/nvim-cmp' },
   config = function()
-    require('nvim-autopairs').setup {}
+    local config = require 'nvim-autopairs'
+    config.setup {
+      check_ts = true, -- enable treesitter
+      ts_config = {
+        lua = { 'string' }, -- it will not add a pair on that treesitter node
+        javascript = { 'template_string' },
+        java = false, -- don't check treesitter on java
+      },
+    }
     -- If you want to automatically add `(` after selecting a function or method
     local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
     local cmp = require 'cmp'
