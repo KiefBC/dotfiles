43549ef5fc816635e9ad280229bf5e19e40a3347
Kiefer <kiefhay642@outlook.com>
2024-12-13 17:59:40 -0800

    Modified various configs of Wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,27 +1,31 @@
+-- Pull in the wezterm API
 local wezterm = require("wezterm")
-local config = {}
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
 config.color_scheme = "Batman"
-config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+-- config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 14
+config.window_decorations = "RESIZE"
 config.line_height = 1.2
-config.background = {
-	{
-		source = { File = "/path/to/image.png" },
-		width = "100%",
-		height = "100%",
-		opacity = 0.1,
-		attachment = { Parallax = 0.1 },
-	},
-}
+config.enable_tab_bar = true
+config.window_background_opacity = 0.9
+-- config.background = {
+-- 	{
+-- 		source = { File = "/path/to/image.png" },
+-- 		width = "100%",
+-- 		height = "100%",
+-- 		opacity = 0.1,
+-- 		attachment = { Parallax = 0.1 },
+-- 	},
+-- }
 local act = wezterm.action
 config.keys = {
 	{ key = "t", mods = "CTRL|SHIFT", action = act.SpawnTab("DefaultDomain") },
 	{ key = "w", mods = "CTRL|SHIFT", action = act.CloseCurrentTab({ confirm = true }) },
+	-- This will close Wezterm
+	{ key = "q", mods = "CTRL|SHIFT", action = act.Quit },
 }
-
--- Function to show Hello message
-wezterm.on("window-config-reloaded", function(window, pane)
-	window:toast_notification("Welcome", "Hello! Welcome to WezTerm!", nil, 3000)
-end)
 
 return config
