4995e49394428ed4ff6c705b4ad94dec20bfb23f
Kiefer <kiefhay642@outlook.com>
2024-12-13 20:11:58 -0800

    Fixed the Mason issue. It was due to loading.

--- nvim/.config/nvim/lua/custom/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp/mason.lua
@@ -1,38 +1,45 @@
 return {
-  'williamboman/mason.nvim',
-  dependencies = {
-    'williamboman/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
+  -- Load mason.nvim first
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require('mason').setup {}
+    end,
   },
-  config = function()
-    local config = require 'mason'
-    local mason_lspconfig = require 'mason-lspconfig'
-    
-    config.setup {}
-    mason_lspconfig.setup {
-      ensure_installed = {
-        'lua_ls',
-        'bashls',
-        'ts_ls',
-        'svelte',
-        'tailwindcss',
-        'html',
-        'cssls',
-        'emmet_ls',
-      },
-    }
-
-    local mason_tool_installer = require 'mason-tool-installer'
-
-    mason_tool_installer.setup {
-      ensure_installed = {
-        'stylua',
-        'ruff',
-        'prettierd',
-        'shfmt',
-        'pylint',
-        'eslint_d',
-      },
-    }
-  end,
+  -- Then load mason-lspconfig.nvim after mason.nvim
+  {
+    'williamboman/mason-lspconfig.nvim',
+    dependencies = { 'williamboman/mason.nvim' },
+    config = function()
+      require('mason-lspconfig').setup {
+        ensure_installed = {
+          'lua_ls',
+          'bashls',
+          'ts_ls',
+          'svelte',
+          'tailwindcss',
+          'html',
+          'cssls',
+          'emmet_ls',
+        },
+      }
+    end,
+  },
+  -- Finally, load mason-tool-installer.nvim after mason.nvim
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    dependencies = { 'williamboman/mason.nvim' },
+    config = function()
+      require('mason-tool-installer').setup {
+        ensure_installed = {
+          'stylua',
+          'ruff',
+          'prettierd',
+          'shfmt',
+          'pylint',
+          'eslint_d',
+        },
+      }
+    end,
+  },
 }
