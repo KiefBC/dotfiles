17293d4e0c117f436940351d3974c37ac91a7041
Kiefer <kiefhay642@outlook.com>
2024-12-13 20:12:07 -0800

    Fixed Lazy

--- nvim/.config/nvim/lua/custom/lazy.lua
+++ nvim/.config/nvim/lua/custom/lazy.lua
@@ -8,7 +8,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({ {  import = 'custom.plugins' }, {import = 'custom.plugins.lsp'} }, {
+require('lazy').setup({ { import = 'custom.plugins' }, { import = 'custom.plugins.lsp' } }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
