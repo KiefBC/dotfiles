ca52f96c8fbd7927b4e6f448c6c04211c8c9b76a
Kiefer <kiefhay642@outlook.com>
2024-12-19 11:31:42 -0800

    Added some better Rust plugins and Treesitter syntax highlighting

--- nvim/.config/nvim/lua/custom/plugins/rustacean.lua
+++ nvim/.config/nvim/lua/custom/plugins/rustacean.lua
@@ -1,6 +1,12 @@
 return {
   {
+    -- INFO: Do not add this to MAson or LSP Config
+    -- Rustacean does all of this for us.
+    -- https://github.com/mrcjkb/rustaceanvim
     'mrcjkb/rustaceanvim',
+    depdencies = {
+      'adaszko/tree_climber_rust.nvim',
+    },
     version = '^5',
     lazy = false,
     ['rust-analyzer'] = {
@@ -8,5 +14,32 @@ return {
         allFeatures = true,
       },
     },
+    server = {
+      on_attach = function(client, bufnr)
+        local opts = { noremap = true, silent = true }
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', 's', '<cmd>lua require("tree_climber_rust").init_selection()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'x', 's', '<cmd>lua require("tree_climber_rust").select_incremental()<CR>', opts)
+        vim.api.nvim_buf_set_keymap(bufnr, 'x', 'S', '<cmd>lua require("tree_climber_rust").select_previous()<CR>', opts)
+      end,
+    },
+  },
+  {
+    -- INFO: This is for managing out Rust crates
+    -- https://github.com/Saecki/crates.nvim
+    {
+      'saecki/crates.nvim',
+      event = { 'BufRead Cargo.toml' },
+      config = function()
+        require('crates').setup()
+      end,
+    },
+  },
+  {
+    -- INFO: This addeds better highlighting for Rust
+    -- https://github.com/adaszko/tree_climber_rust.nvim
+    'adaszko/tree_climber_rust.nvim',
+    -- config = function()
+    --   require('tree_climber_rust').setup()
+    -- end,
   },
 }
