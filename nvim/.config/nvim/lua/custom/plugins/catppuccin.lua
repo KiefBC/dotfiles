ec327bb9625e80119e1c6d04e8a97a6577f501af
Kiefer <kiefhay642@outlook.com>
2024-12-22 22:38:56 -0800

    Modified

--- nvim/.config/nvim/lua/custom/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/custom/plugins/catppuccin.lua
@@ -5,6 +5,14 @@ return {
     require('catppuccin').setup {
       flavour = 'mocha', -- choose your Catppuccin flavor: latte, frappe, macchiato, mocha
       transparent_background = true,
+      integrations = {
+        blink_cmp = true,
+        dashboard = true,
+        fzf = true,
+        mason = true,
+        noice = true,
+        notifier = true,
+      },
     }
     vim.cmd 'colorscheme catppuccin'
   end,
