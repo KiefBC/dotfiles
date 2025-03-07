889eaf6515ca92702aacd87cea35e5612512c13f
Kiefer <kiefhay642@outlook.com>
2025-03-06 23:38:14 -0800

    Modified to work with Mac

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,42 +1,26 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- config.color_scheme = "Batman"
 config.color_scheme = "Catppuccin Mocha"
--- config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 14
 config.window_decorations = "RESIZE"
 config.line_height = 1.2
 config.enable_tab_bar = true
 config.window_background_opacity = 0.95
--- config.win32_system_backdrop = "Acrylic" INFO: Not sure if i like this effect or not.
+config.macos_window_background_blur = 20
 
--- config.background = {
--- 	{
--- 		source = { File = "/path/to/image.png" },
--- 		width = "100%",
--- 		height = "100%",
--- 		opacity = 0.1,
--- 		attachment = { Parallax = 0.1 },
--- 	},
--- }
 local act = wezterm.action
 config.keys = {
-	{ key = "t", mods = "CTRL|SHIFT", action = act.SpawnTab("DefaultDomain") },
-	{ key = "w", mods = "CTRL|SHIFT", action = act.CloseCurrentTab({ confirm = true }) },
+  { key = "t", mods = "CTRL|SHIFT", action = act.SpawnTab("DefaultDomain") },
+  { key = "w", mods = "CTRL|SHIFT", action = act.CloseCurrentTab({ confirm = true }) },
 }
 
--- Retrieve the default WSL domains
-local wsl_domains = wezterm.default_wsl_domains()
-
--- Set the WSL domains
-config.wsl_domains = wsl_domains
-
--- Set the default domain to your preferred WSL distribution
--- Replace 'WSL:Ubuntu' with the actual name of your distribution
-config.default_domain = "WSL:Ubuntu"
+-- Only set WSL-specific configuration on Windows
+if wezterm.target_triple:find("windows") then
+  local wsl_domains = wezterm.default_wsl_domains()
+  config.wsl_domains = wsl_domains
+  config.default_domain = "WSL:Ubuntu"
+end
 
 return config
