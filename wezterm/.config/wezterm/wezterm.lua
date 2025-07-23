94bd69e248b8ff9148f1487035e07bc0d7b80c0a
KiefBC <kiefhay642@outlook.com>
2025-07-22 23:18:43 -0700

    modified theme, formatting
    Signed-off-by: KiefBC <kiefhay642@outlook.com>

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,8 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Catppuccin Mocha"
+config.colors = require("cyberdream")
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 14
 config.window_decorations = "RESIZE"
@@ -12,15 +13,15 @@ config.macos_window_background_blur = 20
 
 local act = wezterm.action
 config.keys = {
-  { key = "t", mods = "CTRL|SHIFT", action = act.SpawnTab("DefaultDomain") },
-  { key = "w", mods = "CTRL|SHIFT", action = act.CloseCurrentTab({ confirm = true }) },
+	{ key = "t", mods = "CTRL|SHIFT", action = act.SpawnTab("DefaultDomain") },
+	{ key = "w", mods = "CTRL|SHIFT", action = act.CloseCurrentTab({ confirm = true }) },
 }
 
 -- Only set WSL-specific configuration on Windows
 if wezterm.target_triple:find("windows") then
-  local wsl_domains = wezterm.default_wsl_domains()
-  config.wsl_domains = wsl_domains
-  config.default_domain = "WSL:Ubuntu"
+	local wsl_domains = wezterm.default_wsl_domains()
+	config.wsl_domains = wsl_domains
+	config.default_domain = "WSL:Ubuntu"
 end
 
 return config
