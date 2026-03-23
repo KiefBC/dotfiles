554e71038cfea1f7b34afc140f5608f8e23e8f37
KiefBC <kiefhay642@outlook.com>
2026-03-22 18:43:28 -0700

    chore: changed cyberdream and removed discord rich pres

--- nvim/.config/nvim/lua/plugins/cyberdream.lua
+++ nvim/.config/nvim/lua/plugins/cyberdream.lua
@@ -2,5 +2,19 @@ return {
   'scottmckendry/cyberdream.nvim',
   lazy = false,
   priority = 1000,
-  vim.cmd 'colorscheme cyberdream',
+  config = function()
+    require('cyberdream').setup {
+      -- Use solid background
+      transparent = false,
+
+      -- Visual enhancements
+      italic_comments = true,
+
+      -- Terminal colors
+      terminal_colors = true,
+    }
+
+    -- Set as active colorscheme
+    vim.cmd 'colorscheme cyberdream'
+  end,
 }
